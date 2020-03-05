3902afd08abe5a5f7c43679a67fda59f05ea9397
s19322 <s19322@pjwstk.edu.pl>
2020-03-05 11:12:12 +0100

    finished

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();//czeka na pobranie calej strony
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+
+            }
+
+
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
